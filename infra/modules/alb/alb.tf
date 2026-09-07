97ef4451a5f650dcff4f1a3a032535afc70ee376
kriskashi <justkristenhaslam@gmail.com>
2026-09-07 15:51:15 +0100

    ALB first draft, missing ACM

---
+++ infra/modules/alb/alb.tf
@@ -0,0 +1,55 @@
+resource "aws_lb" "alb" {
+  name               = "alb"
+  load_balancer_type = "application"
+  subnets = [aws_subnet.subnet-1.id,aws_subnet.subnet-2.id]
+  security_groups = [aws_security_group.web-sg.id,aws_security_group.self-rf.id]
+  
+
+}
+
+
+
+
+
+resource "aws_lb_target_group" "tg-ip" {
+  name        = "tg"
+  port        = 8080
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+  health_check {
+    path = "/health"
+
+
+  }
+}
+
+
+resource "aws_lb_listener" "listener-https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   =  ## ACM !!!!
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg-ip.arn
+  }
+}
+
+resource "aws_lb_listener" "listener-http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"               # This listener forces https by redirecting http traffic to https
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
