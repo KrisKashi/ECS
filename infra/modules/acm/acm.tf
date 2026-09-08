5b49a7feea5581b2b0f6ca3bd6ce299f20af8a63
kriskashi <justkristenhaslam@gmail.com>
2026-09-08 15:35:13 +0100

    ACM cloudflare/aws integration

--- infra/modules/acm/acm.tf
+++ infra/modules/acm/acm.tf
@@ -10,7 +10,7 @@ resource "aws_acm_certificate" "cert" {
 
 resource "cloudflare_dns_record" "kristendns" { # use cloudflare provider to validate ownership to issue cert
   zone_id = var.cloudflare_zone_id
-  name = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
+  name = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name # takes first value from list of record names created from ACM which will be the one we just requested
   ttl = 3600
   type = "CNAME"
   comment = "Domain verification record"
@@ -30,3 +30,7 @@ resource "cloudflare_dns_record" "alb-dns" { # points domain to alb DNS
   tags = ["alb-dns"]
 }
 
+resource "aws_acm_certificate_validation" "dns" { #checks to see if the record exists
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns =[aws_acm_certificate.cert.domain_validation_options[0].resource_record_name] #Hostname declared explicitly as theres reported bugs in the clouudflare terraform module referencing.
+}
