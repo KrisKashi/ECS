ce0a52e205ed81f120b42486906960aae13ef57b
kriskashi <justkristenhaslam@gmail.com>
2026-09-08 15:33:55 +0100

    ACM cloudflare/aws integration

---
+++ infra/modules/acm/acm.tf
@@ -0,0 +1,32 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "kristenhaslam.com"
+  validation_method = "DNS"                 # request to issue a cert 
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "cloudflare_dns_record" "kristendns" { # use cloudflare provider to validate ownership to issue cert
+  zone_id = var.cloudflare_zone_id
+  name = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
+  ttl = 3600
+  type = "CNAME"
+  comment = "Domain verification record"
+  content = aws_acm_certificate.cert.domain_validation_options[0].resource_record_value
+  proxied = false
+  tags = ["cloudflare-dns"]
+}
+
+resource "cloudflare_dns_record" "alb-dns" { # points domain to alb DNS
+  zone_id = var.cloudflare_zone_id
+  name = "kristenhaslam.com"
+  ttl = 3600
+  type = "CNAME"
+  comment = "ALB dns"
+  content = aws_lb.alb.dns_name
+  proxied = false
+  tags = ["alb-dns"]
+}
+
