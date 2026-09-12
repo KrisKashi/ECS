a97aef68fd5143a20b33d1db685b9d0db60a7ec1
KrisKashi <justkristenhaslam@gmail.com>
2026-09-12 19:31:39 +0100

    added updated provider file to github

--- infra/provider.tf
+++ infra/provider.tf
@@ -4,17 +4,25 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
   }
 
-backend "s3" {
-    bucket = "gatus-tfstate"
-    key    = "terraform.tfstate"
-    region = "eu-west-2"
+  backend "s3" {
+    bucket       = "gatus-tfstate"
+    key          = "terraform.tfstate"
+    region       = "eu-west-2"
     use_lockfile = true
-    }
+  }
 }
 
-# Configure the AWS Provider
+# Configure Providers
 provider "aws" {
-    region = "eu-west-2"
+  region = "eu-west-2"
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_token
 }
