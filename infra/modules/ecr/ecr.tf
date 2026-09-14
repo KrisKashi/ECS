d1bf5d657e9f3228ad016151b308da9db4399b39
kriskashi <justkristenhaslam@gmail.com>
2026-09-14 09:57:07 +0100

    added precise versioning in modules and got rid of unused variables

--- infra/modules/ecr/ecr.tf
+++ infra/modules/ecr/ecr.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_version = ">= 1.15"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
+
 resource "aws_ecr_repository" "gatus-image" {
   name                 = "gatus-tf"
   image_tag_mutability = "MUTABLE"
